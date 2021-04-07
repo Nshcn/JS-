f50393fd7ecc74484ae807c042ee728d7da9b852
Bill Zhan <2695563464@qq.com>
2021-04-07 15:13:44 +0800

    实现方块下落，并保留地图状态

--- js/Block.js
+++ js/Block.js
@@ -28,4 +28,41 @@
       }
     }
   };
+  // 能力判断方法，判读的是对应位置的方块和地图是否有都不为0的情况，如果有返回true,否则返回false
+  Block.prototype.check = function (row, col) {
+    // check函数的row和col指的是要校验的地图的row和col的位置
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 4; j++) {
+        if (this.code[i][j] != 0 && game.map.mapCode[row + i][col + j] !== 0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+  // 方块下落，需要判断当前的方块能否下落
+  Block.prototype.checkDown = function () {
+    // 判断当前地图的位置和自己方块的位置是否有重合，this.row + 1指的是预判断
+    // 预判断就是在下一次方块将要到达的位置是否有对应的地图不为0
+    if (this.check(this.row + 1, this.col)) {
+      this.row++;
+    } else {
+      // 此时就是下落到底的状态，生成新方块
+      game.block = new Block();
+      // 方块已经到底了，然后要渲染到地图的code中
+      this.renderMap();
+    }
+  };
+  // 将已经到底的方块渲染到地图中
+  Block.prototype.renderMap = function () {
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 4; j++) {
+        //   将现在已有的方块渲染到Map类的mapCode上
+        if (this.code[i][j] != 0) {
+          // 改变地图的mapCode数据
+          game.map.mapCode[this.row + i][this.col + j] = this.code[i][j];
+        }
+      }
+    }
+  };
 })();
